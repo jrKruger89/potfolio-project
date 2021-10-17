a8842a8db0fb7a528554243af669d3ddc6b54e98
jrKruger89 <35734267+jrKruger89@users.noreply.github.com>
2021-10-17 17:58:32 +0200

    structure work on home screen

--- js/main.js
+++ js/main.js
@@ -4,13 +4,16 @@
 const burger = document.querySelector(".burger");
 const navlist = document.querySelector(".nav-list");
 const open = document.querySelector(".open");
+const main = document.querySelector("main");
 
 let toggleMenu = () => {
   burger.classList.toggle("open");
 
   if (burger.classList.contains("open")) {
     navlist.style.left = "0px";
+    main.classList.add("blur");
   } else {
-    navlist.style.left = "-150px";
+    navlist.style.left = "-200px";
+    main.classList.remove("blur");
   }
 };
